1a537c9f31758bc96b5ac6557ded66bed8c192cd
Samet Demiralay <demiralayahmetsamet@gmail.com>
2022-08-25 19:29:42 +0300

    create components and fetch parts

--- NEREDE_KALDIM.md
+++ NEREDE_KALDIM.md
@@ -1,3 +1,3 @@
-39:04
+1:08:30
 
 https://www.youtube.com/watch?v=Tdp_S_72mSI
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,29 @@
+import React from "react";
 import { NextPage } from "next";
+// Fetch Hook
+import { useFetchMovies } from "../api/fetchHooks";
+// components
 import Header from "../components/Header/Header";
+import Hero from "../components/Hero/Hero";
+import Grid from "../components/Grid/Grid";
+import Card from "../components/Card/Card";
+import Spinner from "../components/Spinner/Spinner";
 
 const Home: NextPage = () => {
+  const [query, setQuery] = React.useState("");
+
+  const { data, fetchNextPage, isLoading, isFetching, error } =
+    useFetchMovies(query);
+
+  console.log(data);
+
   return (
     <main className='relative h-screen overscroll-y-scroll'>
       <Header />
-      <p>imdb</p>
+      <Hero />
+      <Grid />
+      <Card />
+      <Spinner />
     </main>
   );
 };
